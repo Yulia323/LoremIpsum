b4c4b347d0b2c86c6cb5816a8193f95ce8c2a49b
Yulia Balyakina <yuliaxbalyakina@gmail.com>
2023-06-02 22:46:04 +0300

    fix

--- src/main.js
+++ src/main.js
@@ -1,27 +1,27 @@
-const sliderPercent = document.getElementById('sliderPercent');
-const select = document.getElementById('select');
+const rangePercent = document.getElementById('rangePercent');
+const orderSelect = document.getElementById('orderSelect');
 const currentSelect = document.getElementById('selectCurrent');
 
 function init() {
-    document.getElementById('selectBody').addEventListener('click', e => optionSelected(e))
+    document.getElementById('selectList').addEventListener('click', e => optionSelected(e))
     document.getElementById('selectHeader').addEventListener('click', selectOpen)
-    document.getElementById('sliderInput').addEventListener('input', (e) => {
-        sliderPercent.innerText = e.target.value + '%'
+    document.getElementById('rangeInput').addEventListener('input', (e) => {
+        rangePercent.innerText = e.target.value + '%'
     })
 
     //input type file
     const inputFile = document.getElementById('inputFile');
-    const inputLabel = document.getElementById('inputLabel');
+    const inputText = document.getElementById('inputFileText');
 
     inputFile.addEventListener('change', function () {
         if (this.files && this.files.length >= 1)
-            inputLabel.innerText = 'Прикреплено файлов: ' + this.files.length;
+            inputText.innerText = 'Прикреплено файлов: ' + this.files.length;
     });
     submitForm()
 }
 
 function submitForm() {
-    const form = document.getElementById('form-order');
+    const form = document.getElementById('formOrder');
     form.addEventListener('submit', e => {
         e.preventDefault();
         const formData = new FormData(form);
@@ -31,16 +31,15 @@ function submitForm() {
 
 //custom select
 function selectOpen() {
-    select.classList.toggle('select-active')
+    orderSelect.classList.toggle('select__header__active')
 }
 
 function optionSelected(e) {
 
     if (e.target && e.target.tagName === 'LI') {
         currentSelect.innerHTML = e.target.innerText
-        select.classList.toggle('select-active')
+        orderSelect.classList.toggle('select__header__active')
     }
 }
-
 
 init()
